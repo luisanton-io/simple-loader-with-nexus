2d0ff3281ffb3ed6ab9cdb3581c2a0f73bb39f85
Luis Antonio Canettoli Ordoñez <hello@luisanton.io>
2021-08-21 15:33:46 +0200

    Updated to recoil-nexus 0.3.0

--- src/toggleLoading.ts
+++ src/toggleLoading.ts
@@ -1,9 +1,9 @@
 import { getRecoil, setRecoil } from "recoil-nexus";
 import { loadingState } from "atoms/loadingState";
 
-export const toggleLoading = async () => {
+export const toggleLoading = () => {
 
-    const loading = await getRecoil(loadingState);
+    const loading = getRecoil(loadingState);
     setRecoil(loadingState, !loading);
 
 };
