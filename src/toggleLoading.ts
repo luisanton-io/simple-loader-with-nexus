a50f2b88464a66884cbb2cc695769f15d75a0365
Luis Antonio Canettoli Ordoñez <hello@luisanton.io>
2022-07-07 21:16:19 +0400

    Update toggleLoading.ts

--- src/toggleLoading.ts
+++ src/toggleLoading.ts
@@ -2,8 +2,8 @@ import { getRecoil, setRecoil } from "recoil-nexus";
 import { loadingState } from "atoms/loadingState";
 
 export const toggleLoading = () => {
-
     const loading = getRecoil(loadingState);
-    setRecoil(loadingState, !loading);
-
+    console.log("Before", loading);
+    setRecoil(loadingState, (l) => !l);
+    console.log("After", getRecoil(loadingState));
 };
